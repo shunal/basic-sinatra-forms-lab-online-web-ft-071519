3e32a79bbdfb4ce6184b5119840e18bb95ab5457
Steven Hunal <stevehunal@gmail.com>
2019-09-19 01:30:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,12 @@ get '/newteam' do
     original = params["string"]
     
     @name = params["name"]
-    
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @c = params["c"]
     erb :team
     
   end
